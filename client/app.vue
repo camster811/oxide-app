115af21058602cf7968d6b85fc569d2725d6fafd
Ihce <dylan.stancil@outlook.com>
2024-07-22 17:13:33 -0500

    moved to vue

--- client/app.vue
+++ client/app.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import MegaMenu from 'primevue/megamenu';
+import { ref } from 'vue';
 
 const [modules, collections] = await Promise.all([
-    $fetch('http://localhost:8000/api/modules/get'),
-    $fetch('http://localhost:8000/api/collections/get')
-  ])
+    fetch('http://localhost:8000/api/modules/get').then(res => res.json()),
+    fetch('http://localhost:8000/api/collections/get').then(res => res.json())
+])
+const selectedCollection = ref();
 console.log(modules)
 </script>
 
 <template>
-  <MegaMenu :model="collections" orientation="vertical">
-    <template #collection="{ collection }"></template>
-  </MegaMenu>
+  <div class="card flex justify-center">
+    <div class="inline-block">
+      <Listbox v-model="selectedCollection" :options="collections" filter optionlabel="" class="md:w-56" />
+    </div>
+  </div>
 </template>
-
