e46f26213a6fb85c2192b808f36f8b9849719453
Camster811 <cls0150@auburn.edu>
2024-09-10 15:32:54 -0500

    create control_flow_graph module and add to index

--- client/functions/index.js
+++ client/functions/index.js
@@ -5,5 +5,6 @@ export { default as blockLenHistogram } from './block_len_histogram';
 export { default as opcodeHistogram } from './opcode_histogram';
 export { default as opcodeNgramsHeatmap } from './opcode_ngrams';
 export { default as callGraphModule } from './call_graph';
+export { default as flowGraph } from './control_flow_graph';
 
 
